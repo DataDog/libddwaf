5305130362b13808dcfeaa6509031d3e85ca2518
Anil Mahtani <929854+Anilm3@users.noreply.github.com>
2022-02-17 13:31:09 +0000

    Add __STDC_FORMAT_MACROS to ensure PRIu64 is available on older platforms

--- src/log.hpp
+++ src/log.hpp
@@ -8,6 +8,10 @@
 #define DDWAF_LOG_H
 
 #include <ddwaf.h>
+
+// Required for PRIu64 format specifier in older systems
+#define __STDC_FORMAT_MACROS
+
 #include <cinttypes>
 #include <string>
 #include <type_traits>
