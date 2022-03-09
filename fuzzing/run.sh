d7dac8792d06fc475257b1b9feda58fa389177f0
Anil Mahtani <929854+Anilm3@users.noreply.github.com>
2022-03-09 14:58:21 +0000

    Update fuzzer default to 60, fix job naming on action

--- fuzzing/run.sh
+++ fuzzing/run.sh
@@ -10,6 +10,5 @@ export ASAN_OPTIONS=detect_leaks=1
 
 rm -f fuzz-*.log
 
-echo "Run fuzzer for ${1:-30} seconds"
-./fuzzer -timeout=0.1 -report_slow_units=0.01 -max_total_time=${1:-30} -max_len=1000 -rss_limit_mb=4096 -use_value_profile=1 -dict=sample_dict.txt -artifact_prefix=results/ -jobs=4 -workers=4 -reload=0 corpus
-# ./fuzzer -timeout=0.1 -report_slow_units=0.01 -max_total_time=${1:-30} -max_len=1000 -rss_limit_mb=4096 -use_value_profile=1 -dict=sample_dict.txt -artifact_prefix=results/ -reload=0 corpus
+echo "Run fuzzer for ${1:-60} seconds"
+./fuzzer -timeout=0.1 -report_slow_units=0.01 -max_total_time=${1:-60} -max_len=1000 -rss_limit_mb=4096 -use_value_profile=1 -dict=sample_dict.txt -artifact_prefix=results/ -jobs=4 -workers=4 -reload=0 corpus
