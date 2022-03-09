113c29a68eb11d4097a526acaa51e14a73f18b92
Anil Mahtani <929854+Anilm3@users.noreply.github.com>
2022-03-09 16:51:03 +0000

    Release 1.1.0 (unstable) (#59)

--- tests/TestInterface.cpp
+++ tests/TestInterface.cpp
@@ -295,8 +295,8 @@ TEST(FunctionalTests, ddwaf_get_version)
     ddwaf_get_version(&version);
 
     EXPECT_EQ(version.major, 1);
-    EXPECT_EQ(version.minor, 0);
-    EXPECT_EQ(version.patch, 18);
+    EXPECT_EQ(version.minor, 1);
+    EXPECT_EQ(version.patch, 0);
 }
 
 TEST(FunctionalTests, ddwaf_runNull)
