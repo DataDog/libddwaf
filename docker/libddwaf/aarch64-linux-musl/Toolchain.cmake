05b02f560f27f9b83397e5d2ec0f1035aafe1b7e
Loic Nageleisen <loic.nageleisen@datadoghq.com>
2022-05-18 14:00:25 +0200

    Add per libc linux builds
    Adds support for building libddwaf for multiple platforms and against
    different libcs in a controlled environment, whether run in CI, on
    Linux, or on macOS, and whether on an ARM or Intel machine.
    
    This makes it seem that there are a lot of combinations, but each one of
    them proves to be useful in different scenarios:
    
    - builds native to the targeted libc (glibc built on Debian, musl built
      on Alpine)
    - symmetric, uniform experience whether the user is on an ARM (e.g Apple
      Silicon) or Intel machine
    - fast cross-compiling to foreign platform
    - ability to build as if on a foreign platform (albeit slow, via qemu)
    
    Not all of them have to be run in CI, only those that aim to produce
    publishable artifacts. Nonetheless, every case has proven to be useful
    in practice, and this persists self-documenting, reproducible tooling
    that I previously had to piece together over and over again.
    
    Also, updated docker/README.md with valuable information (see Per-libc
    linux builds).

---
+++ docker/libddwaf/aarch64-linux-musl/Toolchain.cmake
@@ -0,0 +1,8 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR aarch64)
+set(CMAKE_C_COMPILER aarch64-alpine-linux-musl-gcc)
+set(CMAKE_CXX_COMPILER aarch64-alpine-linux-musl-g++)
+set(CMAKE_FIND_ROOT_PATH /usr/aarch64-alpine-linux-musl)
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
