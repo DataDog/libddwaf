aaf72fc9e3f149584bf91174bd81c33702d3f7f9
Loic Nageleisen <loic.nageleisen@datadoghq.com>
2022-07-29 17:21:25 +0200

    Simplify portable builds
    As portable builds were iteratively improved over time they accreted a
    lot of cruft. Using the per libc builds as a baseline, cross-compiling
    on glibc-based Debian to a musl whether targeting the same CPU arch on a
    foreign one has been cleaned up.
    
    This also removes the need to download a custom gcc build and only
    depends on Debian-packaged compilers.
    
    This makes the `docker/libddwaf/x86_64`, `docker/libddwaf/aarch64`, and
    `docker/libddwaf/common` directories largely obsolete.

---
+++ docker/libddwaf/x86_64-linux/Toolchain.x86_64.cmake
@@ -0,0 +1,23 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR x86_64)
+set(CMAKE_C_COMPILER /usr/bin/clang-11)
+set(CMAKE_CXX_COMPILER /usr/bin/clang++-11)
+
+set(CMAKE_AR /usr/bin/llvm-ar-11)
+set(CMAKE_NM /usr/bin/llvm-nm-11)
+set(CMAKE_RANLIB /usr/bin/llvm-ranlib-11)
+set(CMAKE_STRIP /usr/bin/strip) # llvm-strip-11 doesn't seem to work correctly
+
+set(triple x86_64-none-linux-musl)
+set(CMAKE_ASM_COMPILER_TARGET ${triple})
+set(CMAKE_C_COMPILER_TARGET ${triple})
+
+set(CMAKE_SYSROOT /sysroot/musl)
+set(c_cxx_flags "-resource-dir /sysroot/musl -Qunused-arguments -rtlib=compiler-rt -unwindlib=libunwind -static-libgcc")
+set(CMAKE_C_FLAGS ${c_cxx_flags})
+set(CMAKE_CXX_COMPILER_TARGET ${triple})
+set(CMAKE_CXX_FLAGS "-stdlib=libc++ -isystem/sysroot/musl/include/c++/v1 ${c_cxx_flags}")
+
+set(linker_flags "-v -fuse-ld=lld -nodefaultlibs -Wl,-Bstatic -lc++ -lc++abi /sysroot/musl/lib/linux/libclang_rt.builtins-x86_64.a -lunwind -Wl,-Bdynamic -lc /sysroot/musl/lib/linux/libclang_rt.builtins-x86_64.a")
+set(CMAKE_EXE_LINKER_FLAGS_INIT ${linker_flags})
+set(CMAKE_SHARED_LINKER_FLAGS_INIT ${linker_flags})
