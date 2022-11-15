abbf9e80e5e57140d7493b6e09f6878e66921805
Anil Mahtani <929854+Anilm3@users.noreply.github.com>
2022-11-15 14:37:21 +0000

    Run clang tidy / format on CI

---
+++ cmake/clang-format.cmake
@@ -0,0 +1,25 @@
+find_program(CLANG_FORMAT clang-format)
+if(CLANG_FORMAT STREQUAL CLANG_FORMAT-NOTFOUND)
+    message(STATUS "Cannot find clang-format, either set CLANG_FORMAT or make it discoverable")
+    return()
+endif()
+
+set(FILE_LIST "")
+foreach(DIR IN ITEMS src tests validator perf fuzzing)
+    file(GLOB_RECURSE SOURCE_FILES ${DIR}/*.hpp ${DIR}/*.cpp)
+    list(APPEND FILE_LIST ${SOURCE_FILES})
+endforeach()
+
+add_custom_target(format
+    COMMAND ${CLANG_FORMAT} -n -Werror ${FILE_LIST}
+    COMMAND ${CMAKE_SOURCE_DIR}/cmake/check_headers.rb
+    WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
+
+add_custom_target(format_fix
+    COMMAND ${CMAKE_SOURCE_DIR}/cmake/check_headers.rb --fix
+    COMMAND ${CLANG_FORMAT} -i ${FILE_LIST}
+    WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
+
+add_custom_target(format_fix_chg
+    COMMAND bash -c "git status --porcelain=1 :/ | grep -E '\.(c|h|cpp|hpp)$' | awk '{ print \"${CMAKE_SOURCE_DIR}/\" $NF }' | xargs echo '${CLANG_FORMAT}' -i"
+    WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
