feb8a50278716d68bac99b8b605e3d46db806678
Anil Mahtani <929854+Anilm3@users.noreply.github.com>
2023-03-15 10:36:50 +0000

    Remove compat memory resource

--- src/rule.hpp
+++ src/rule.hpp
@@ -6,9 +6,9 @@
 
 #pragma once
 
-#include "compat_memory_resource.hpp"
 #include <atomic>
 #include <memory>
+#include <memory_resource>
 #include <string>
 #include <unordered_map>
 #include <vector>
