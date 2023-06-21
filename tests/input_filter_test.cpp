12de78042027fcc4f9c22832c784c4b6165c224d
Anil Mahtani <929854+Anilm3@users.noreply.github.com>
2023-06-21 16:37:11 +0100

    Add input filter test

--- tests/input_filter_test.cpp
+++ tests/input_filter_test.cpp
@@ -4,6 +4,7 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2021 Datadog, Inc.
 
+#include "PWTransformer.h"
 #include "test.h"
 
 using namespace ddwaf;
@@ -85,6 +86,40 @@ TEST(TestInputFilter, InputExclusionWithCondition)
     ddwaf_object tmp;
     ddwaf_object_map(&root);
     ddwaf_object_map_add(&root, "http.client_ip", ddwaf_object_string(&tmp, "192.168.0.1"));
+
+    ddwaf::object_store store(manifest);
+    store.insert(root);
+
+    auto obj_filter = std::make_shared<object_filter>();
+    obj_filter->insert(client_ip, {});
+    auto rule = std::make_shared<ddwaf::rule>(ddwaf::rule("", "", {}, {}));
+    input_filter filter("filter", std::move(conditions), {rule.get()}, std::move(obj_filter));
+
+    ddwaf::timer deadline{2s};
+    input_filter::cache_type cache;
+
+    auto opt_spec = filter.match(store, cache, deadline);
+    ASSERT_TRUE(opt_spec.has_value());
+    EXPECT_EQ(opt_spec->rules.size(), 1);
+    EXPECT_EQ(opt_spec->objects.size(), 1);
+    EXPECT_NE(opt_spec->objects.find(&root.array[0]), opt_spec->objects.end());
+}
+
+TEST(TestInputFilter, InputExclusionWithConditionAndTransformers)
+{
+    ddwaf::manifest manifest;
+    auto client_ip = manifest.insert("usr.id");
+
+    std::vector<condition::target_type> targets{{client_ip, "usr.id", {}, {PWT_LOWERCASE}}};
+    auto cond = std::make_shared<condition>(std::move(targets),
+        std::make_unique<rule_processor::exact_match>(std::vector<std::string>{"admin"}));
+
+    std::vector<std::shared_ptr<condition>> conditions{std::move(cond)};
+
+    ddwaf_object root;
+    ddwaf_object tmp;
+    ddwaf_object_map(&root);
+    ddwaf_object_map_add(&root, "usr.id", ddwaf_object_string(&tmp, "ADMIN"));
 
     ddwaf::object_store store(manifest);
     store.insert(root);
