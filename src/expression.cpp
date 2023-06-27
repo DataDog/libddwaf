0a90b596638761227a6f9de8fbb10eeb371db588
Anil Mahtani <929854+Anilm3@users.noreply.github.com>
2023-06-27 18:36:34 +0100

    Minor changes

--- src/expression.cpp
+++ src/expression.cpp
@@ -11,22 +11,113 @@
 
 namespace ddwaf {
 
-namespace {
-
-bool evaluate_condition()
+template <typename T>
+std::optional<event::match> expression::eval_target(condition &cond, cache_type &cache, T &it,
+    const rule_processor::base::ptr &processor, const std::vector<PW_TRANSFORM_ID> & /*transformers*/,
+    ddwaf::timer &deadline) const
 {
-    
+    for (; it; ++it) {
+        if (deadline.expired()) {
+            throw ddwaf::timeout_exception();
+        }
+
+        if (it.type() != DDWAF_OBJ_STRING) {
+            continue;
+        }
+
+        auto optional_match = processor->match_object(*it);
+        if (!optional_match.has_value()) {
+            continue;
+        }
+
+        if (!cond.dependents.scalar.empty()) {
+            for (auto index : cond.dependents.scalar) {
+                
+        optional_match->key_path = std::move(it.get_current_path());
+        // If this target matched, we can stop processing
+        return optional_match;
+    }
+
+    return std::nullopt;
 }
 
+
+bool expression::eval_condition(std::size_t index, cache_type &cache, const object_store &store,
+    const std::unordered_set<const ddwaf_object *> &objects_excluded,
+    ddwaf::timer &deadline) const
+{
+    const auto &cond = conditions_[index];
+    auto  &cond_cache = cache.condition_cache[index];
+
+    for (const auto &target : cond.targets) {
+        if (deadline.expired()) {
+            throw ddwaf::timeout_exception();
+        }
+
+        if (target.scope == eval_scope::local ||
+            cond_cache.targets.find(target.root) != cond_cache.targets.end()) {
+            continue;
+        }
+
+        // TODO: iterators could be cached to avoid reinitialisation
+        const auto *object = store.get_target(target.root);
+        if (object == nullptr) {
+            continue;
+        }
+
+        std::optional<event::match> optional_match;
+        if (target.source == data_source::keys) {
+            object::key_iterator it(object, target.key_path, objects_excluded, limits_);
+            optional_match = eval_target(it, cond.processor, target.transformers, deadline);
+        } else {
+            object::value_iterator it(object, target.key_path, objects_excluded, limits_);
+            optional_match = eval_target(it, cond.processor, target.transformers, deadline);
+        }
+
+        if (optional_match.has_value()) {
+            optional_match->address = target.name;
+
+            DDWAF_TRACE("Target %s matched parameter value %s",
+                target.name.c_str(), optional_match->resolved.c_str());
+
+            cond_cache.result = optional_match;
+            return true;
+        }
+    }
+
+    return false;
 }
 
 bool expression::eval(cache_type &cache, const object_store &store,
     const std::unordered_set<const ddwaf_object *> &objects_excluded,
     ddwaf::timer &deadline) const
 {
-    for (unsigned i = 0; i < conditions_.size(); ++i) {
+    for (std::size_t i = 0; i < conditions_.size(); ++i) {
+        auto res = eval_condition(i, cache, store, objects_excluded, deadline);
+    }
+
+    return true;
+}
 
 
+expression::condition::condition(std::vector<expression::target_type> targets_,
+        std::shared_ptr<rule_processor::base> processor_):
+    targets(std::move(targets_)), processor(std::move(processor_))
+{
+    for (const auto &target : targets) {
+        if (target.scope == expression::eval_scope::global) { continue; }
+
+        switch(target.entity) {
+        case expression::eval_entity::resolved:
+            dependents.resolved.emplace(target.condition_index);
+            break;
+        case expression::eval_entity::scalar:
+            dependents.scalar.emplace(target.condition_index);
+            break;
+        case expression::eval_entity::object:
+            dependents.object.emplace(target.condition_index);
+            break;
+        }
     }
 }
 
--- src/expression.hpp
+++ src/expression.hpp
@@ -25,25 +25,25 @@ namespace ddwaf {
 
 class expression {
 public:
+    using ptr = std::shared_ptr<expression>;
+
     enum class data_source : uint8_t { values, keys };
-    enum class address_scope : uint8_t { global, local };
-    enum class eval_target : uint8_t {resolved, scalar, object};
+    enum class eval_scope : uint8_t { global, local };
+    enum class eval_entity : uint8_t {resolved, scalar, object};
 
     struct target_type {
-        address_scope scope{address_scope::global};
+        eval_scope scope{eval_scope::global};
         std::string name;
 
         // Global scope
-        struct {
-            target_index root;
-            std::vector<std::string> key_path{};
-        } global;
+        target_index root;
 
         // Local scope
-        struct {
-            std::size_t index;
-            eval_target target{eval_target::object};
-        } local;
+        std::size_t condition_index;
+        eval_entity entity{eval_entity::object};
+
+        // Applicable to either scope
+        std::vector<std::string> key_path{};
 
         // Transformers
         std::vector<PW_TRANSFORM_ID> transformers{};
@@ -52,12 +52,20 @@ public:
 
     struct condition {
         struct cache_type {
-            bool result{false};
-            memory::unordered_set<target_index> evaluated_targets;
+            memory::unordered_set<target_index> targets;
+            std::optional<event::match> result;
         };
+
+        condition(std::vector<expression::target_type> targets,
+            std::shared_ptr<rule_processor::base> processor);
 
         std::vector<target_type> targets;
         std::shared_ptr<rule_processor::base> processor;
+        struct {
+            std::unordered_set<std::size_t> resolved{};
+            std::unordered_set<std::size_t> scalar{};
+            std::unordered_set<std::size_t> object{};
+        } dependents;
     };
 
     struct eval_result {
@@ -67,26 +75,25 @@ public:
     };
 
     struct cache_type {
-        std::vector<eval_result> results;
         memory::vector<condition::cache_type> condition_cache{};
 
         // NOLINTNEXTLINE(bugprone-easily-swappable-parameters)
-        void cache_result(std::size_t index, target_index target, std::optional<eval_result> &result) {
-            if (index >= condition_cache.size()) { return; }
+/*        void cache_result(std::size_t index, target_index target, std::optional<eval_result> &result) {*/
+            /*if (index >= condition_cache.size()) { return; }*/
 
-            if (result.has_value()) {
-                results[index] = std::move(*result);
-                condition_cache[index].result = true;
-            }
-            condition_cache[index].evaluated_targets.emplace(target);
-        }
+            /*if (result.has_value()) {*/
+                /*results[index] = std::move(*result);*/
+                /*condition_cache[index].result = true;*/
+            /*}*/
+            /*condition_cache[index].targets.emplace(target);*/
+        /*}*/
 
-        void invalidate(std::size_t index) {
-            if (index >= condition_cache.size()) { return; }
+        /*void invalidate(std::size_t index) {*/
+            /*if (index >= condition_cache.size()) { return; }*/
 
-            condition_cache[index].result = false;
-            results[index] = {};
-        }
+            /*condition_cache[index].result = false;*/
+            /*results[index] = {};*/
+        /*}*/
     };
 
     explicit expression(std::vector<condition> &&conditions, ddwaf::object_limits limits = ddwaf::object_limits()):
@@ -97,6 +104,16 @@ public:
         ddwaf::timer &deadline) const;
 
 protected:
+
+    template <typename T>
+    std::optional<event::match> eval_target(condition &cond, cache_type &cache, T &it,
+        const rule_processor::base::ptr &processor, const std::vector<PW_TRANSFORM_ID> &transformers,
+        ddwaf::timer &deadline) const;
+
+    bool eval_condition(std::size_t index, cache_type &cache, const object_store &store,
+        const std::unordered_set<const ddwaf_object *> &objects_excluded,
+        ddwaf::timer &deadline) const;
+
     ddwaf::object_limits limits_;
     std::vector<condition> conditions_;
 };
