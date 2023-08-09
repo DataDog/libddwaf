896c18a838b40e3689f609752037c3b252b8f2a1
Anil Mahtani <929854+Anilm3@users.noreply.github.com>
2023-08-09 22:27:04 +0100

    Revert legacy build changes

--- docker/libddwaf/aarch64/smoketest/smoke_musl_static_libgcc.sh
+++ docker/libddwaf/aarch64/smoketest/smoke_musl_static_libgcc.sh
@@ -5,7 +5,7 @@ mkdir -p musl_static_libgcc
 cd musl_static_libgcc
 cmake -DCMAKE_C_COMPILER=/muslsysroot/bin/musl-gcc \
   -DCMAKE_SYSROOT=/muslsysroot \
-  -DCMAKE_EXE_LINKER_FLAGS="-v -nodefaultlibs -Wl,-Bstatic -lgcc" \
+  -DCMAKE_EXE_LINKER_FLAGS="-v" \
   -DCMAKE_PREFIX_PATH=/muslsysroot/share/cmake/libddwaf \
   -DLINK_DDWAF_STATIC=ON \
   ../../../../../smoketest/
