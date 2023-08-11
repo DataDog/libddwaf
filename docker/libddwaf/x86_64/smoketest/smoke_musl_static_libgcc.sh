26725c81097be423b48dff52214d0648a5c8c9df
Anil Mahtani <929854+Anilm3@users.noreply.github.com>
2023-08-11 12:39:30 +0100

    Pack libc++ and friends in libddwaf.a and strip

--- docker/libddwaf/x86_64/smoketest/smoke_musl_static_libgcc.sh
+++ docker/libddwaf/x86_64/smoketest/smoke_musl_static_libgcc.sh
@@ -7,7 +7,7 @@ cmake -DCMAKE_C_COMPILER=/muslsysroot/bin/musl-gcc \
   -DCMAKE_SYSROOT=/muslsysroot \
   -DCMAKE_EXE_LINKER_FLAGS="-v" \
   -DCMAKE_PREFIX_PATH=/muslsysroot/share/cmake/libddwaf \
-  -DLINK_DDWAF_STATIC=ON \
-  -DLIBDDWAF_SMOKETEST_SYSROOT=/muslsysroot/lib \
+  -DLIBDDWAF_SMOKE_LINK_STATIC=ON \
+  -DLIBDDWAF_SMOKE_LINK_STATIC_FLAGS="-Wl,--push-state -Wl,-L/muslsysroot/lib -l:libc++.a -l:libc++experimental.a -l:libc++abi.a -l:libunwind.a -Wl,--pop-state" \
   ../../../../../smoketest/
 make
