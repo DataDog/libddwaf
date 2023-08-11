f9a8360eceeba6d824f0b7c3af0cdca94075f134
Anil Mahtani <929854+Anilm3@users.noreply.github.com>
2023-08-11 19:59:51 +0100

    Minor fixes

--- src/glibc_compat.c
+++ src/glibc_compat.c
@@ -1,12 +1,20 @@
-#include <features.h>
+// Unless explicitly stated otherwise all files in this repository are
+// dual-licensed under the Apache-2.0 License or BSD-3-Clause License.
+//
+// This product includes software developed at Datadog
+// (https://www.datadoghq.com/). Copyright 2023 Datadog, Inc.
+
 #include <stdlib.h>
-#if !defined(__GLIBC__) && (defined(__ARM_ARCH_7__) || defined(__i386__))
+
+#if defined(__linux__) && !defined(__GLIBC__) && (defined(__arm__) || defined(__i386__))
+
+#include <features.h>
 
 __attribute__((weak))
 int __nanosleep64(void *req, void *rem) {
-	(void) req;
-	(void) rem;
-	abort();
+    (void) req;
+    (void) rem;
+    abort();
 }
 __attribute__((weak))
 int __nanosleep_time64(void *req, void *rem) {
@@ -15,10 +23,10 @@ int __nanosleep_time64(void *req, void *rem) {
 
 __attribute__((weak))
 int __pthread_cond_timedwait64(void *cond, void *mutex, void *abstime) {
-	(void) cond;
-	(void) mutex;
-	(void) abstime;
-	abort();
+    (void) cond;
+    (void) mutex;
+    (void) abstime;
+    abort();
 }
 __attribute__((weak))
 int __pthread_cond_timedwait_time64(void *cond, void *mutex, void *abstime) {
@@ -27,9 +35,9 @@ int __pthread_cond_timedwait_time64(void *cond, void *mutex, void *abstime) {
 
 __attribute__((weak))
 void *dlsym(void * handle, const char *name) {
-	(void) handle;
-	(void) name;
-	abort();
+    (void) handle;
+    (void) name;
+    abort();
 }
 __attribute__((weak))
 void *__dlsym_time64(void *handle, const char *name) {
