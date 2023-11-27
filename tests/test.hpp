f594eb078acdc07f7f1765c7b8b85f87dccb843f
Anil Mahtani <929854+Anilm3@users.noreply.github.com>
2023-11-27 18:15:45 +0000

    Fix formatting

--- tests/test.hpp
+++ tests/test.hpp
@@ -14,29 +14,29 @@
 #define SHORT_TIME 1
 
 #define DDWAF_OBJECT_INVALID                                                                       \
-  {                                                                                                \
-    NULL, 0, {NULL}, 0, DDWAF_OBJ_INVALID                                                          \
-  }
+    {                                                                                              \
+        NULL, 0, {NULL}, 0, DDWAF_OBJ_INVALID                                                      \
+    }
 #define DDWAF_OBJECT_MAP                                                                           \
-  {                                                                                                \
-    NULL, 0, {NULL}, 0, DDWAF_OBJ_MAP                                                              \
-  }
+    {                                                                                              \
+        NULL, 0, {NULL}, 0, DDWAF_OBJ_MAP                                                          \
+    }
 #define DDWAF_OBJECT_ARRAY                                                                         \
-  {                                                                                                \
-    NULL, 0, {NULL}, 0, DDWAF_OBJ_ARRAY                                                            \
-  }
+    {                                                                                              \
+        NULL, 0, {NULL}, 0, DDWAF_OBJ_ARRAY                                                        \
+    }
 #define DDWAF_OBJECT_SIGNED_FORCE(value)                                                           \
-  {                                                                                                \
-    NULL, 0, {(const char *)value}, 0, DDWAF_OBJ_SIGNED                                            \
-  }
+    {                                                                                              \
+        NULL, 0, {(const char *)value}, 0, DDWAF_OBJ_SIGNED                                        \
+    }
 #define DDWAF_OBJECT_UNSIGNED_FORCE(value)                                                         \
-  {                                                                                                \
-    NULL, 0, {(const char *)value}, 0, DDWAF_OBJ_UNSIGNED                                          \
-  }
+    {                                                                                              \
+        NULL, 0, {(const char *)value}, 0, DDWAF_OBJ_UNSIGNED                                      \
+    }
 #define DDWAF_OBJECT_STRING_PTR(string, length)                                                    \
-  {                                                                                                \
-    NULL, 0, {string}, length, DDWAF_OBJ_STRING                                                    \
-  }
+    {                                                                                              \
+        NULL, 0, {string}, length, DDWAF_OBJ_STRING                                                \
+    }
 
 #define EXPECT_STR(a, b) EXPECT_STREQ(a.c_str(), b)
 #define EXPECT_STRV(a, b) EXPECT_STREQ(a.data(), b)
