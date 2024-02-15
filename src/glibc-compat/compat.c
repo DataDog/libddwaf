9d263db43b9fbaa2d2ff53f43d76ce47caf78d45
Anil Mahtani <929854+Anilm3@users.noreply.github.com>
2024-02-15 16:13:56 +0000

    Use dlsym to load an available ceilf

--- src/glibc-compat/compat.c
+++ src/glibc-compat/compat.c
@@ -47,21 +47,22 @@ void *__dlsym_time64(void *handle, const char *name) {
 
 #if defined(__linux__)
 
+#include <stdint.h>
+#include <stdlib.h>
+
+#ifndef __USE_GNU
+#define __USE_GNU
+#endif
+#include <dlfcn.h>
+
 #if defined(__aarch64__)
 // Extracted from https://git.musl-libc.org/cgit/musl/tree/src/math/aarch64/ceilf.c
-
-__attribute__((weak))
-float ceilf(float x)
+static float ceilf_local(float x)
 {
-	__asm__ ("frintp %s0, %s1" : "=w"(x) : "w"(x));
-	return x;
+        __asm__ ("frintp %s0, %s1" : "=w"(x) : "w"(x));
+        return x;
 }
 #else
-
-#include <stdint.h>
-
-// Extracted from https://git.musl-libc.org/cgit/musl/tree/src/math/ceilf.c
-
 /* fp_force_eval ensures that the input value is computed when that's
    otherwise unused.  To prevent the constant folding of the input
    expression, an additional fp_barrier may be needed or a compilation
@@ -75,35 +76,55 @@ static inline void fp_force_evalf(float x)
     (void)y;
 }
 
+static float ceilf_local(float x)
+{
+        // Extracted from https://git.musl-libc.org/cgit/musl/tree/src/math/ceilf.c
+        union {float f; uint32_t i;} u = {x};
+        int e = (int)(u.i >> 23 & 0xff) - 0x7f;
+        uint32_t m;
+
+        if (e >= 23) {
+            return x;
+        }
+        if (e >= 0) {
+            m = 0x007fffff >> e;
+            if ((u.i & m) == 0) {
+                return x;
+            }
+            fp_force_evalf(x + 0x1p120f);
+            if (u.i >> 31 == 0){
+                u.i += m;
+            }
+            u.i &= ~m;
+        } else {
+            fp_force_evalf(x + 0x1p120f);
+            if (u.i >> 31) {
+                u.f = -0.0;
+        } else if (u.i << 1) {
+                u.f = 1.0;
+            }
+        }
+        return u.f;
+}
+#endif
+
+#define unlikely(x)    __builtin_expect(!!(x), 0)
+
+typedef float (*ceilf_t)(float);
+
+static ceilf_t ceilf_global_;
+
 __attribute__((weak))
 float ceilf(float x)
 {
-    union {float f; uint32_t i;} u = {x};
-    int e = (int)(u.i >> 23 & 0xff) - 0x7f;
-    uint32_t m;
-
-    if (e >= 23) {
-        return x;
-    }
-    if (e >= 0) {
-        m = 0x007fffff >> e;
-        if ((u.i & m) == 0) {
-            return x;
-        }
-        fp_force_evalf(x + 0x1p120f);
-        if (u.i >> 31 == 0){
-            u.i += m;
-        }
-        u.i &= ~m;
-    } else {
-        fp_force_evalf(x + 0x1p120f);
-        if (u.i >> 31) {
-            u.f = -0.0;
-    } else if (u.i << 1) {
-            u.f = 1.0;
+    if (unlikely(ceilf_global_ == NULL)) {
+        void *ceilf_sym = dlsym(RTLD_NEXT, "ceilf");
+        if (ceilf_sym == NULL) {
+            ceilf_global_ = &ceilf_local;
+        } else {
+            ceilf_global_ = (ceilf_t)ceilf_sym;
         }
     }
-    return u.f;
+    return ceilf_global_(x);
 }
-#endif
 #endif
