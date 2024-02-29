8cee0e56de09381ee96b438269675dd84690fea5
Anil Mahtani <929854+Anilm3@users.noreply.github.com>
2024-02-29 11:42:42 +0000

    fuzzer improvements

--- fuzzer/global/scripts/show_coverage.sh
+++ fuzzer/global/scripts/show_coverage.sh
@@ -4,12 +4,12 @@ set -eu
 cd fuzzer/global
 
 llvm-profdata-17 merge -sparse *.profraw -o default.profdata
-llvm-cov-17 show fuzzer -instr-profile=default.profdata -ignore-filename-regex="(vendor|fuzzer|third_party)" -format=html > coverage.html
-llvm-cov-17 report -instr-profile default.profdata fuzzer -ignore-filename-regex="(vendor|fuzzer|third_party)" -show-region-summary=false
+llvm-cov-17 show global_fuzzer -instr-profile=default.profdata -ignore-filename-regex="(vendor|fuzzer|third_party)" -format=html > coverage.html
+llvm-cov-17 report -instr-profile default.profdata global_fuzzer -ignore-filename-regex="(vendor|fuzzer|third_party)" -show-region-summary=false
 
 if [ ! -z ${1:-} ]; then 
     THRESHOLD=$1
-    TOTAL=$(llvm-cov-17 report -instr-profile default.profdata fuzzer -ignore-filename-regex="(vendor|fuzzer|third_party)" -show-region-summary=false | grep TOTAL)
+    TOTAL=$(llvm-cov-17 report -instr-profile default.profdata global_fuzzer -ignore-filename-regex="(vendor|fuzzer|third_party)" -show-region-summary=false | grep TOTAL)
     ARRAY=($TOTAL)
     COVERAGE=$(echo ${ARRAY[3]} | sed -e "s/\.[[:digit:]]*%//g")
 
