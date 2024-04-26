5a3dde377ec3f9e46bf52806d0c560e21a474fc0
Anil Mahtani <929854+Anilm3@users.noreply.github.com>
2024-04-26 16:35:20 +0100

    Use __builtin_ceilf and add badges to readme

--- src/glibc-compat/math.c
+++ src/glibc-compat/math.c
@@ -13,58 +13,10 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-#if defined(__aarch64__)
-// Extracted from https://git.musl-libc.org/cgit/musl/tree/src/math/aarch64/ceilf.c
 static float ceilf_local(float x)
 {
-        __asm__ ("frintp %s0, %s1" : "=w"(x) : "w"(x));
-        return x;
+    return __builtin_ceilf(x);
 }
-#else
-/* fp_force_eval ensures that the input value is computed when that's
-   otherwise unused.  To prevent the constant folding of the input
-   expression, an additional fp_barrier may be needed or a compilation
-   mode that does so (e.g. -frounding-math in gcc). Then it can be
-   used to evaluate an expression for its fenv side-effects only.   */
-
-static inline void fp_force_evalf(float x)
-{
-    volatile float y;
-    y = x;
-    (void)y;
-}
-
-static float ceilf_local(float x)
-{
-        // Extracted from https://git.musl-libc.org/cgit/musl/tree/src/math/ceilf.c
-        union {float f; uint32_t i;} u = {x};
-        int e = (int)(u.i >> 23 & 0xff) - 0x7f;
-        uint32_t m;
-
-        if (e >= 23) {
-            return x;
-        }
-        if (e >= 0) {
-            m = 0x007fffff >> e;
-            if ((u.i & m) == 0) {
-                return x;
-            }
-            fp_force_evalf(x + 0x1p120f);
-            if (u.i >> 31 == 0){
-                u.i += m;
-            }
-            u.i &= ~m;
-        } else {
-            fp_force_evalf(x + 0x1p120f);
-            if (u.i >> 31) {
-                u.f = -0.0;
-        } else if (u.i << 1) {
-                u.f = 1.0;
-            }
-        }
-        return u.f;
-}
-#endif
 
 #define unlikely(x)    __builtin_expect(!!(x), 0)
 
