53c3a860540a63bc3d5fdb5356249f7eaa76ffde
Anil Mahtani <929854+Anilm3@users.noreply.github.com>
2024-06-24 17:49:54 +0100

    Add one more test

--- tests/integration/exclusion_data/test.cpp
+++ tests/integration/exclusion_data/test.cpp
@@ -12,7 +12,7 @@ using namespace ddwaf;
 namespace {
 constexpr std::string_view base_dir = "integration/exclusion_data/";
 
-TEST(TestExclusionDataIntegration, ExcludeByUserID)
+TEST(TestExclusionDataIntegration, ExcludeRuleByUserID)
 {
     auto rule = read_file("exclude_one_rule_by_user.yaml", base_dir);
     ASSERT_TRUE(rule.type != DDWAF_OBJ_INVALID);
@@ -143,7 +143,7 @@ TEST(TestExclusionDataIntegration, ExcludeByUserID)
     ddwaf_destroy(handle3);
 }
 
-TEST(TestExclusionDataIntegration, ExcludeByClientIP)
+TEST(TestExclusionDataIntegration, ExcludeRuleByClientIP)
 {
     auto rule = read_file("exclude_one_rule_by_ip.yaml", base_dir);
     ASSERT_TRUE(rule.type != DDWAF_OBJ_INVALID);
@@ -358,6 +358,136 @@ TEST(TestExclusionDataIntegration, UnknownDataTypeOnExclusionData)
     {
         auto root =
             yaml_to_object(R"({exclusion_data: [{id: ip_data, type: unknown_data, data: [{}]}]})");
+
+        handle3 = ddwaf_update(handle1, &root, nullptr);
+        ASSERT_NE(handle3, nullptr);
+        ddwaf_object_free(&root);
+    }
+
+    {
+        ddwaf_context context = ddwaf_context_init(handle3);
+        ASSERT_NE(context, nullptr);
+
+        ddwaf_object root;
+        ddwaf_object tmp;
+        ddwaf_object_map(&root);
+        ddwaf_object_map_add(&root, "http.client_ip", ddwaf_object_string(&tmp, "192.168.0.1"));
+        ddwaf_object_map_add(&root, "usr.id", ddwaf_object_string(&tmp, "admin"));
+
+        ddwaf_result out;
+        EXPECT_EQ(ddwaf_run(context, &root, nullptr, &out, LONG_TIME), DDWAF_MATCH);
+        EXPECT_EVENTS(out,
+            {.id = "1",
+                .name = "rule1",
+                .tags = {{"type", "type1"}, {"category", "category"}},
+                .matches = {{.op = "exact_match",
+                    .highlight = "admin",
+                    .args = {{
+                        .value = "admin",
+                        .address = "usr.id",
+                    }}}}},
+            {.id = "2",
+                .name = "rule2",
+                .tags = {{"type", "type2"}, {"category", "category"}},
+                .matches = {{.op = "exact_match",
+                    .highlight = "admin",
+                    .args = {{
+                        .value = "admin",
+                        .address = "usr.id",
+                    }}}}});
+        ddwaf_result_free(&out);
+        ddwaf_context_destroy(context);
+    }
+
+    ddwaf_destroy(handle1);
+    ddwaf_destroy(handle2);
+    ddwaf_destroy(handle3);
+}
+
+TEST(TestExclusionDataIntegration, ExcludeInputByClientIP)
+{
+    auto rule = read_file("exclude_one_input_by_ip.yaml", base_dir);
+    ASSERT_TRUE(rule.type != DDWAF_OBJ_INVALID);
+
+    ddwaf_handle handle1 = ddwaf_init(&rule, nullptr, nullptr);
+    ASSERT_NE(handle1, nullptr);
+    ddwaf_object_free(&rule);
+
+    {
+        ddwaf_context context = ddwaf_context_init(handle1);
+        ASSERT_NE(context, nullptr);
+
+        ddwaf_object root;
+        ddwaf_object tmp;
+        ddwaf_object_map(&root);
+        ddwaf_object_map_add(&root, "http.client_ip", ddwaf_object_string(&tmp, "192.168.0.1"));
+        ddwaf_object_map_add(&root, "usr.id", ddwaf_object_string(&tmp, "admin"));
+
+        ddwaf_result out;
+        EXPECT_EQ(ddwaf_run(context, &root, nullptr, &out, LONG_TIME), DDWAF_MATCH);
+        EXPECT_EVENTS(out,
+            {.id = "1",
+                .name = "rule1",
+                .tags = {{"type", "type1"}, {"category", "category"}},
+                .matches = {{.op = "exact_match",
+                    .highlight = "admin",
+                    .args = {{
+                        .value = "admin",
+                        .address = "usr.id",
+                    }}}}},
+            {.id = "2",
+                .name = "rule2",
+                .tags = {{"type", "type2"}, {"category", "category"}},
+                .matches = {{.op = "exact_match",
+                    .highlight = "admin",
+                    .args = {{
+                        .value = "admin",
+                        .address = "usr.id",
+                    }}}}});
+
+        ddwaf_result_free(&out);
+        ddwaf_context_destroy(context);
+    }
+
+    ddwaf_handle handle2;
+    {
+        auto root = yaml_to_object(
+            R"({exclusion_data: [{id: ip_data, type: ip_with_expiration, data: [{value: 192.168.0.1, expiration: 0}]}]})");
+
+        handle2 = ddwaf_update(handle1, &root, nullptr);
+        ASSERT_NE(handle2, nullptr);
+        ddwaf_object_free(&root);
+    }
+
+    {
+        ddwaf_context context = ddwaf_context_init(handle2);
+        ASSERT_NE(context, nullptr);
+
+        ddwaf_object root;
+        ddwaf_object tmp;
+        ddwaf_object_map(&root);
+        ddwaf_object_map_add(&root, "http.client_ip", ddwaf_object_string(&tmp, "192.168.0.1"));
+        ddwaf_object_map_add(&root, "usr.id", ddwaf_object_string(&tmp, "admin"));
+
+        ddwaf_result out;
+        EXPECT_EQ(ddwaf_run(context, &root, nullptr, &out, LONG_TIME), DDWAF_MATCH);
+        EXPECT_EVENTS(out, {.id = "2",
+                               .name = "rule2",
+                               .tags = {{"type", "type2"}, {"category", "category"}},
+                               .matches = {{.op = "exact_match",
+                                   .highlight = "admin",
+                                   .args = {{
+                                       .value = "admin",
+                                       .address = "usr.id",
+                                   }}}}});
+
+        ddwaf_result_free(&out);
+        ddwaf_context_destroy(context);
+    }
+
+    ddwaf_handle handle3;
+    {
+        auto root = yaml_to_object(R"({exclusion_data: []})");
 
         handle3 = ddwaf_update(handle1, &root, nullptr);
         ASSERT_NE(handle3, nullptr);
