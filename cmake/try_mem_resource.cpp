41eacc890d46f0b5bc27ae05e513989ca94c33e5
Anil Mahtani <929854+Anilm3@users.noreply.github.com>
2024-10-14 19:34:17 +0100

    Add cmake checks

---
+++ cmake/try_mem_resource.cpp
@@ -0,0 +1,7 @@
+#include <memory_resource>
+
+#if !defined(__cpp_lib_memory_resource)
+#  error "No memory resource available"
+#endif
+
+int main() {}
---
+++ cmake/try_mem_resource_exp.cpp
@@ -0,0 +1,7 @@
+#include <experimental/memory_resource>
+
+#if !defined(__cpp_lib_experimental_memory_resources)
+#  error "No memory resources available"
+#endif
+
+int main() {}
