27a4a218f3ecd21d2343f2f25004f8a8305a7500
Anil Mahtani <929854+Anilm3@users.noreply.github.com>
2024-10-16 22:11:17 +0100

    Add missing header

--- src/condition/shi_detector.cpp
+++ src/condition/shi_detector.cpp
@@ -3,6 +3,7 @@
 //
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2021 Datadog, Inc.
+#include <algorithm>
 #include <cstddef>
 #include <optional>
 #include <string>
