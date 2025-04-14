3b6fc2d91f9976440f2f3ab6663f226687c4d438
Anil Mahtani <929854+Anilm3@users.noreply.github.com>
2025-04-14 16:59:27 +0100

    Minor change for older compilers

--- src/configuration/processor_override_parser.cpp
+++ src/configuration/processor_override_parser.cpp
@@ -38,7 +38,9 @@ processor_override_spec parse_override(const raw_configuration::map &node)
         }
     } else {
         // Since the target array is empty, the ID is mandatory
-        current.targets.emplace_back(reference_type::id, at<std::string>(node, "id"), decltype(reference_spec::tags){});
+        // NOLINTNEXTLINE(hicpp-use-emplace,modernize-use-emplace)
+        current.targets.emplace_back(reference_spec{
+            .type = reference_type::id, .ref_id = at<std::string>(node, "id"), .tags = {}});
     }
 
     auto scanners_target_array = at<raw_configuration::vector>(node, "scanners", {});
