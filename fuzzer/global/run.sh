afd228a6fe3a9859c0e40f00cd6521889fc1f2c6
Anil Mahtani <929854+Anilm3@users.noreply.github.com>
2025-04-16 15:58:38 +0100

    Final fix to false positive leak debacle

--- fuzzer/global/run.sh
+++ fuzzer/global/run.sh
@@ -6,9 +6,9 @@ python3 fuzzer/global/scripts/build_corpus.py
 
 cd fuzzer/global
 
-export ASAN_OPTIONS="detect_leaks=0,use_stacks=0,verbosity=1,leak_check_at_exit=1"
+export ASAN_OPTIONS="detect_leaks=1"
 
 rm -f fuzz-*.log
 
 echo "Run global fuzzer for ${1:-60} seconds"
-./global_fuzzer -timeout=0.1 -report_slow_units=0.01 -max_total_time=${1:-60} -max_len=1000 -rss_limit_mb=4096 -use_value_profile=1 -dict=sample_dict.txt -artifact_prefix=results/ -jobs=4 -workers=4 -reload=0 corpus
+./global_fuzzer -timeout=0.1 -report_slow_units=0.01 -max_total_time=${1:-60} -max_len=1000 -rss_limit_mb=4096 -use_value_profile=1 -dict=sample_dict.txt -artifact_prefix=results/ -jobs=4 -workers=4 -reload=0 -detect_leaks=0 corpus
