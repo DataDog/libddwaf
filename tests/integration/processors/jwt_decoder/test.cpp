6eee88f4e89632516bafaa3c66b559453e1488bd
Anil Mahtani <929854+Anilm3@users.noreply.github.com>
2025-04-27 20:40:32 +0100

    Integration tests

--- tests/integration/processors/jwt_decoder/test.cpp
+++ tests/integration/processors/jwt_decoder/test.cpp
@@ -11,6 +11,65 @@ using namespace ddwaf;
 namespace {
 constexpr std::string_view base_dir = "integration/processors/jwt_decoder";
 
+TEST(TestJwtDecoderIntegration, Preprocessor)
+{
+    auto rule = read_json_file("preprocessor.json", base_dir);
+    ASSERT_TRUE(rule.type != DDWAF_OBJ_INVALID);
+    ddwaf_handle handle = ddwaf_init(&rule, nullptr, nullptr);
+    ASSERT_NE(handle, nullptr);
+    ddwaf_object_free(&rule);
+
+    uint32_t size;
+    const char *const *addresses = ddwaf_known_addresses(handle, &size);
+    EXPECT_EQ(size, 2);
+    std::unordered_set<std::string_view> address_set(addresses, addresses + size);
+    EXPECT_TRUE(address_set.contains("server.request.headers.no_cookies"));
+    EXPECT_TRUE(address_set.contains("server.request.jwt"));
+
+    ddwaf_context context = ddwaf_context_init(handle);
+    ASSERT_NE(context, nullptr);
+
+    ddwaf_object tmp;
+
+    ddwaf_object map = DDWAF_OBJECT_MAP;
+
+    ddwaf_object headers;
+    ddwaf_object_map(&headers);
+    ddwaf_object_map_add(&headers, "authorization",
+        ddwaf_object_string(&tmp,
+            "Bearer "
+            "eyJhbGciOiJSUzM4NCIsInR5cCI6IkpXVCJ9."
+            "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUx"
+            "NjIzOTAyMn0.o1hC1xYbJolSyh0-bOY230w22zEQSk5TiBfc-OCvtpI2JtYlW-23-"
+            "8B48NpATozzMHn0j3rE0xVUldxShzy0xeJ7vYAccVXu2Gs9rnTVqouc-UZu_wJHkZiKBL67j8_"
+            "61L6SXswzPAQu4kVDwAefGf5hyYBUM-80vYZwWPEpLI8K4yCBsF6I9N1yQaZAJmkMp_"
+            "Iw371Menae4Mp4JusvBJS-s6LrmG2QbiZaFaxVJiW8KlUkWyUCns8-"
+            "qFl5OMeYlgGFsyvvSHvXCzQrsEXqyCdS4tQJd73ayYA4SPtCb9clz76N1zE5WsV4Z0BYrxeb77oA7jJh"
+            "h994RAPzCG0hmQ"));
+
+    ddwaf_object_map_add(&map, "server.request.headers.no_cookies", &headers);
+
+    ddwaf_result out;
+    ASSERT_EQ(ddwaf_run(context, &map, nullptr, &out, LONG_TIME), DDWAF_MATCH);
+    EXPECT_FALSE(out.timeout);
+
+    EXPECT_EVENTS(out, {.id = "rule1",
+                           .name = "rule1",
+                           .tags = {{"type", "flow1"}, {"category", "category1"}},
+                           .matches = {{.op = "equals",
+                               .args = {{
+                                   .value = "RS384",
+                                   .address = "server.request.jwt",
+                                   .path = {"header", "alg"},
+                               }}}}});
+
+    EXPECT_EQ(ddwaf_object_size(&out.derivatives), 0);
+
+    ddwaf_result_free(&out);
+    ddwaf_context_destroy(context);
+    ddwaf_destroy(handle);
+}
+
 TEST(TestJwtDecoderIntegration, Postprocessor)
 {
     auto rule = read_json_file("postprocessor.json", base_dir);
@@ -35,22 +94,84 @@ TEST(TestJwtDecoderIntegration, Postprocessor)
     ddwaf_object headers;
     ddwaf_object_map(&headers);
     ddwaf_object_map_add(&headers, "authorization",
-        ddwaf_object_string(
-            &tmp, "Bearer "
-                  "eyJhbGciOiJSUzM4NCIsInR5cCI6IkpXVCJ9."
-                  "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUx"
-                  "NjIzOTAyMn0.o1hC1xYbJolSyh0-bOY230w22zEQSk5TiBfc-OCvtpI2JtYlW-23-"
-                  "8B48NpATozzMHn0j3rE0xVUldxShzy0xeJ7vYAccVXu2Gs9rnTVqouc-UZu_wJHkZiKBL67j8_"
-                  "61L6SXswzPAQu4kVDwAefGf5hyYBUM-80vYZwWPEpLI8K4yCBsF6I9N1yQaZAJmkMp_"
-                  "Iw371Menae4Mp4JusvBJS-s6LrmG2QbiZaFaxVJiW8KlUkWyUCns8-"
-                  "qFl5OMeYlgGFsyvvSHvXCzQrsEXqyCdS4tQJd73ayYA4SPtCb9clz76N1zE5WsV4Z0BYrxeb77oA7jJh"
-                  "h994RAPzCG0hmQ"));
+        ddwaf_object_string(&tmp,
+            "Bearer "
+            "eyJhbGciOiJSUzM4NCIsInR5cCI6IkpXVCJ9."
+            "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUx"
+            "NjIzOTAyMn0.o1hC1xYbJolSyh0-bOY230w22zEQSk5TiBfc-OCvtpI2JtYlW-23-"
+            "8B48NpATozzMHn0j3rE0xVUldxShzy0xeJ7vYAccVXu2Gs9rnTVqouc-UZu_wJHkZiKBL67j8_"
+            "61L6SXswzPAQu4kVDwAefGf5hyYBUM-80vYZwWPEpLI8K4yCBsF6I9N1yQaZAJmkMp_"
+            "Iw371Menae4Mp4JusvBJS-s6LrmG2QbiZaFaxVJiW8KlUkWyUCns8-"
+            "qFl5OMeYlgGFsyvvSHvXCzQrsEXqyCdS4tQJd73ayYA4SPtCb9clz76N1zE5WsV4Z0BYrxeb77oA7jJh"
+            "h994RAPzCG0hmQ"));
 
     ddwaf_object_map_add(&map, "server.request.headers.no_cookies", &headers);
 
     ddwaf_result out;
     ASSERT_EQ(ddwaf_run(context, &map, nullptr, &out, LONG_TIME), DDWAF_OK);
     EXPECT_FALSE(out.timeout);
+
+    EXPECT_EQ(ddwaf_object_size(&out.derivatives), 1);
+
+    EXPECT_JSON(out.derivatives,
+        R"({"server.request.jwt":{"header":{"alg":"RS384","typ":"JWT"},"payload":{"sub":"1234567890","name":"John Doe","admin":true,"iat":1516239022},"signature":true}})");
+
+    ddwaf_result_free(&out);
+    ddwaf_context_destroy(context);
+    ddwaf_destroy(handle);
+}
+
+TEST(TestJwtDecoderIntegration, Processor)
+{
+    auto rule = read_json_file("processor.json", base_dir);
+    ASSERT_TRUE(rule.type != DDWAF_OBJ_INVALID);
+    ddwaf_handle handle = ddwaf_init(&rule, nullptr, nullptr);
+    ASSERT_NE(handle, nullptr);
+    ddwaf_object_free(&rule);
+
+    uint32_t size;
+    const char *const *addresses = ddwaf_known_addresses(handle, &size);
+    EXPECT_EQ(size, 2);
+    std::unordered_set<std::string_view> address_set(addresses, addresses + size);
+    EXPECT_TRUE(address_set.contains("server.request.headers.no_cookies"));
+    EXPECT_TRUE(address_set.contains("server.request.jwt"));
+
+    ddwaf_context context = ddwaf_context_init(handle);
+    ASSERT_NE(context, nullptr);
+
+    ddwaf_object tmp;
+
+    ddwaf_object map = DDWAF_OBJECT_MAP;
+
+    ddwaf_object headers;
+    ddwaf_object_map(&headers);
+    ddwaf_object_map_add(&headers, "authorization",
+        ddwaf_object_string(&tmp,
+            "Bearer "
+            "eyJhbGciOiJSUzM4NCIsInR5cCI6IkpXVCJ9."
+            "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUx"
+            "NjIzOTAyMn0.o1hC1xYbJolSyh0-bOY230w22zEQSk5TiBfc-OCvtpI2JtYlW-23-"
+            "8B48NpATozzMHn0j3rE0xVUldxShzy0xeJ7vYAccVXu2Gs9rnTVqouc-UZu_wJHkZiKBL67j8_"
+            "61L6SXswzPAQu4kVDwAefGf5hyYBUM-80vYZwWPEpLI8K4yCBsF6I9N1yQaZAJmkMp_"
+            "Iw371Menae4Mp4JusvBJS-s6LrmG2QbiZaFaxVJiW8KlUkWyUCns8-"
+            "qFl5OMeYlgGFsyvvSHvXCzQrsEXqyCdS4tQJd73ayYA4SPtCb9clz76N1zE5WsV4Z0BYrxeb77oA7jJh"
+            "h994RAPzCG0hmQ"));
+
+    ddwaf_object_map_add(&map, "server.request.headers.no_cookies", &headers);
+
+    ddwaf_result out;
+    ASSERT_EQ(ddwaf_run(context, &map, nullptr, &out, LONG_TIME), DDWAF_MATCH);
+    EXPECT_FALSE(out.timeout);
+
+    EXPECT_EVENTS(out, {.id = "rule1",
+                           .name = "rule1",
+                           .tags = {{"type", "flow1"}, {"category", "category1"}},
+                           .matches = {{.op = "equals",
+                               .args = {{
+                                   .value = "RS384",
+                                   .address = "server.request.jwt",
+                                   .path = {"header", "alg"},
+                               }}}}});
 
     EXPECT_EQ(ddwaf_object_size(&out.derivatives), 1);
 
