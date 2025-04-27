1e1606c0be523688c6f790dcf810b9c3bf34d10c
Anil Mahtani <929854+Anilm3@users.noreply.github.com>
2025-04-27 17:34:03 +0100

    Format

--- tests/unit/processor/jwt_decoder_test.cpp
+++ tests/unit/processor/jwt_decoder_test.cpp
@@ -20,15 +20,15 @@ TEST(TestJwtDecoder, Basic)
     ddwaf_object headers;
     ddwaf_object_map(&headers);
     ddwaf_object_map_add(&headers, "authorization",
-        ddwaf_object_string(
-            &tmp, "eyJhbGciOiJSUzM4NCIsInR5cCI6IkpXVCJ9."
-                  "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUx"
-                  "NjIzOTAyMn0.o1hC1xYbJolSyh0-bOY230w22zEQSk5TiBfc-OCvtpI2JtYlW-23-"
-                  "8B48NpATozzMHn0j3rE0xVUldxShzy0xeJ7vYAccVXu2Gs9rnTVqouc-UZu_wJHkZiKBL67j8_"
-                  "61L6SXswzPAQu4kVDwAefGf5hyYBUM-80vYZwWPEpLI8K4yCBsF6I9N1yQaZAJmkMp_"
-                  "Iw371Menae4Mp4JusvBJS-s6LrmG2QbiZaFaxVJiW8KlUkWyUCns8-"
-                  "qFl5OMeYlgGFsyvvSHvXCzQrsEXqyCdS4tQJd73ayYA4SPtCb9clz76N1zE5WsV4Z0BYrxeb77oA7jJh"
-                  "h994RAPzCG0hmQ"));
+        ddwaf_object_string(&tmp,
+            "eyJhbGciOiJSUzM4NCIsInR5cCI6IkpXVCJ9."
+            "eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUx"
+            "NjIzOTAyMn0.o1hC1xYbJolSyh0-bOY230w22zEQSk5TiBfc-OCvtpI2JtYlW-23-"
+            "8B48NpATozzMHn0j3rE0xVUldxShzy0xeJ7vYAccVXu2Gs9rnTVqouc-UZu_wJHkZiKBL67j8_"
+            "61L6SXswzPAQu4kVDwAefGf5hyYBUM-80vYZwWPEpLI8K4yCBsF6I9N1yQaZAJmkMp_"
+            "Iw371Menae4Mp4JusvBJS-s6LrmG2QbiZaFaxVJiW8KlUkWyUCns8-"
+            "qFl5OMeYlgGFsyvvSHvXCzQrsEXqyCdS4tQJd73ayYA4SPtCb9clz76N1zE5WsV4Z0BYrxeb77oA7jJh"
+            "h994RAPzCG0hmQ"));
 
     jwt_decoder gen{"id", {}, {}, false, true};
 
