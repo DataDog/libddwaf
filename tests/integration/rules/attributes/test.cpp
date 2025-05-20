e36fae0df457b34c25e7815f88be5e3d874751fd
Anil Mahtani <929854+Anilm3@users.noreply.github.com>
2025-05-20 14:52:35 +0100

    Add tests with ephemeral parameters

--- tests/integration/rules/attributes/test.cpp
+++ tests/integration/rules/attributes/test.cpp
@@ -524,4 +524,193 @@ TEST(TestRuleAttributesIntegration, AttributesAndBlockingRuleFilter)
     ddwaf_context_destroy(context1);
 }
 
+TEST(TestRuleAttributesIntegration, AttributesAndEphemeralMatches)
+{
+    auto rule = read_file("rules.yaml", base_dir);
+    ASSERT_TRUE(rule.type != DDWAF_OBJ_INVALID);
+
+    ddwaf_handle handle = ddwaf_init(&rule, nullptr, nullptr);
+    ASSERT_NE(handle, nullptr);
+    ddwaf_object_free(&rule);
+
+    ddwaf_context context1 = ddwaf_context_init(handle);
+    ASSERT_NE(context1, nullptr);
+
+    // Destroying the handle should not invalidate it
+    ddwaf_destroy(handle);
+
+    ddwaf_object tmp;
+    {
+        // No Match
+        ddwaf_object parameter = DDWAF_OBJECT_MAP;
+        ddwaf_object_map_add(&parameter, "value1", ddwaf_object_string(&tmp, "rule8"));
+
+        ddwaf_object result;
+        EXPECT_EQ(ddwaf_run(context1, nullptr, &parameter, &result, LONG_TIME), DDWAF_OK);
+
+        EXPECT_EQ(ddwaf_object_type(&result), DDWAF_OBJ_MAP);
+
+        const auto *events = ddwaf_object_find(&result, STRL("events"));
+        EXPECT_NE(events, nullptr);
+        EXPECT_EQ(ddwaf_object_type(events), DDWAF_OBJ_ARRAY);
+        EXPECT_EQ(ddwaf_object_size(events), 0);
+
+        const auto *attributes = ddwaf_object_find(&result, STRL("attributes"));
+        EXPECT_NE(attributes, nullptr);
+        EXPECT_EQ(ddwaf_object_type(attributes), DDWAF_OBJ_MAP);
+        EXPECT_EQ(ddwaf_object_size(attributes), 0);
+
+        ddwaf_object_free(&result);
+    }
+
+    {
+        // The first match should contain attributes
+        ddwaf_object parameter = DDWAF_OBJECT_MAP;
+        ddwaf_object_map_add(&parameter, "value1", ddwaf_object_string(&tmp, "rule1"));
+
+        ddwaf_object result;
+        EXPECT_EQ(ddwaf_run(context1, nullptr, &parameter, &result, LONG_TIME), DDWAF_MATCH);
+
+        EXPECT_EQ(ddwaf_object_type(&result), DDWAF_OBJ_MAP);
+
+        const auto *events = ddwaf_object_find(&result, STRL("events"));
+        EXPECT_NE(events, nullptr);
+        EXPECT_EQ(ddwaf_object_type(events), DDWAF_OBJ_ARRAY);
+        EXPECT_EQ(ddwaf_object_size(events), 0);
+
+        const auto *attributes = ddwaf_object_find(&result, STRL("attributes"));
+        EXPECT_NE(attributes, nullptr);
+        EXPECT_EQ(ddwaf_object_type(attributes), DDWAF_OBJ_MAP);
+        EXPECT_EQ(ddwaf_object_size(attributes), 1);
+
+        ddwaf_object_free(&result);
+    }
+
+    {
+        // The second time around there should be no match as the rules generates no
+        // events and the attributes have already been provided
+        ddwaf_object parameter = DDWAF_OBJECT_MAP;
+        ddwaf_object_map_add(&parameter, "value1", ddwaf_object_string(&tmp, "rule1"));
+
+        ddwaf_object result;
+        EXPECT_EQ(ddwaf_run(context1, nullptr, &parameter, &result, LONG_TIME), DDWAF_OK);
+
+        EXPECT_EQ(ddwaf_object_type(&result), DDWAF_OBJ_MAP);
+
+        const auto *events = ddwaf_object_find(&result, STRL("events"));
+        EXPECT_NE(events, nullptr);
+        EXPECT_EQ(ddwaf_object_type(events), DDWAF_OBJ_ARRAY);
+        EXPECT_EQ(ddwaf_object_size(events), 0);
+
+        const auto *attributes = ddwaf_object_find(&result, STRL("attributes"));
+        EXPECT_NE(attributes, nullptr);
+        EXPECT_EQ(ddwaf_object_type(attributes), DDWAF_OBJ_MAP);
+        EXPECT_EQ(ddwaf_object_size(attributes), 0);
+
+        ddwaf_object_free(&result);
+    }
+
+    ddwaf_context_destroy(context1);
+}
+
+TEST(TestRuleAttributesIntegration, AttributesEventsAndEphemeralMatches)
+{
+    auto rule = read_file("rules.yaml", base_dir);
+    ASSERT_TRUE(rule.type != DDWAF_OBJ_INVALID);
+
+    ddwaf_handle handle = ddwaf_init(&rule, nullptr, nullptr);
+    ASSERT_NE(handle, nullptr);
+    ddwaf_object_free(&rule);
+
+    ddwaf_context context1 = ddwaf_context_init(handle);
+    ASSERT_NE(context1, nullptr);
+
+    // Destroying the handle should not invalidate it
+    ddwaf_destroy(handle);
+
+    ddwaf_object tmp;
+    {
+        // No Match
+        ddwaf_object parameter = DDWAF_OBJECT_MAP;
+        ddwaf_object_map_add(&parameter, "value2", ddwaf_object_string(&tmp, "rule8"));
+
+        ddwaf_object result;
+        EXPECT_EQ(ddwaf_run(context1, nullptr, &parameter, &result, LONG_TIME), DDWAF_OK);
+
+        EXPECT_EQ(ddwaf_object_type(&result), DDWAF_OBJ_MAP);
+
+        const auto *events = ddwaf_object_find(&result, STRL("events"));
+        EXPECT_NE(events, nullptr);
+        EXPECT_EQ(ddwaf_object_type(events), DDWAF_OBJ_ARRAY);
+        EXPECT_EQ(ddwaf_object_size(events), 0);
+
+        const auto *attributes = ddwaf_object_find(&result, STRL("attributes"));
+        EXPECT_NE(attributes, nullptr);
+        EXPECT_EQ(ddwaf_object_type(attributes), DDWAF_OBJ_MAP);
+        EXPECT_EQ(ddwaf_object_size(attributes), 0);
+
+        ddwaf_object_free(&result);
+    }
+
+    {
+        // The first match should contain attributes
+        ddwaf_object parameter = DDWAF_OBJECT_MAP;
+        ddwaf_object_map_add(&parameter, "value2", ddwaf_object_string(&tmp, "rule2"));
+
+        ddwaf_object result;
+        EXPECT_EQ(ddwaf_run(context1, nullptr, &parameter, &result, LONG_TIME), DDWAF_MATCH);
+
+        EXPECT_EQ(ddwaf_object_type(&result), DDWAF_OBJ_MAP);
+
+        EXPECT_EVENTS(result, {.id = "rule2",
+                                  .name = "rule2",
+                                  .tags = {{"type", "flow2"}, {"category", "category2"}},
+                                  .matches = {{.op = "match_regex",
+                                      .op_value = "^rule2",
+                                      .highlight = "rule2"sv,
+                                      .args = {{
+                                          .value = "rule2"sv,
+                                          .address = "value2",
+                                      }}}}});
+
+        const auto *attributes = ddwaf_object_find(&result, STRL("attributes"));
+        EXPECT_NE(attributes, nullptr);
+        EXPECT_EQ(ddwaf_object_type(attributes), DDWAF_OBJ_MAP);
+        EXPECT_EQ(ddwaf_object_size(attributes), 1);
+
+        ddwaf_object_free(&result);
+    }
+
+    {
+        // The second match should contain an event but no attributes
+        ddwaf_object parameter = DDWAF_OBJECT_MAP;
+        ddwaf_object_map_add(&parameter, "value2", ddwaf_object_string(&tmp, "rule2"));
+
+        ddwaf_object result;
+        EXPECT_EQ(ddwaf_run(context1, nullptr, &parameter, &result, LONG_TIME), DDWAF_MATCH);
+
+        EXPECT_EQ(ddwaf_object_type(&result), DDWAF_OBJ_MAP);
+
+        EXPECT_EVENTS(result, {.id = "rule2",
+                                  .name = "rule2",
+                                  .tags = {{"type", "flow2"}, {"category", "category2"}},
+                                  .matches = {{.op = "match_regex",
+                                      .op_value = "^rule2",
+                                      .highlight = "rule2"sv,
+                                      .args = {{
+                                          .value = "rule2"sv,
+                                          .address = "value2",
+                                      }}}}});
+
+        const auto *attributes = ddwaf_object_find(&result, STRL("attributes"));
+        EXPECT_NE(attributes, nullptr);
+        EXPECT_EQ(ddwaf_object_type(attributes), DDWAF_OBJ_MAP);
+        EXPECT_EQ(ddwaf_object_size(attributes), 0);
+
+        ddwaf_object_free(&result);
+    }
+
+    ddwaf_context_destroy(context1);
+}
+
 } // namespace
