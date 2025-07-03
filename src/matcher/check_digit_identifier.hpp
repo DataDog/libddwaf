63f250d58383d5cfce8f8f482b9d9fb094dfc787
Anil Mahtani <929854+Anilm3@users.noreply.github.com>
2025-07-03 14:36:20 +0100

    Check digit matcher with luhn algorithm

---
+++ src/matcher/check_digit_identifier.hpp
@@ -0,0 +1,59 @@
+// Unless explicitly stated otherwise all files in this repository are
+// dual-licensed under the Apache-2.0 License or BSD-3-Clause License.
+//
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2021 Datadog, Inc.
+
+#pragma once
+
+#include <memory>
+#include <re2/re2.h>
+
+#include "matcher/base.hpp"
+
+namespace ddwaf::matcher {
+
+enum class check_digit_algorithm : uint8_t { luhn };
+
+inline check_digit_algorithm cda_from_string(std::string_view str)
+{
+    if (str == "luhn") {
+        return check_digit_algorithm::luhn;
+    }
+
+    throw std::invalid_argument("unknown check digit algorithm");
+}
+
+// Exposed for testing
+bool is_luhn_identifier(std::string_view str);
+
+class check_digit_identifier : public base_impl<check_digit_identifier> {
+public:
+    static constexpr std::string_view matcher_name = "check_digit_identifier";
+    static constexpr std::string_view negated_matcher_name = "!check_digit_identifier";
+
+    check_digit_identifier(check_digit_algorithm cda, const std::string &regex_str,
+        std::size_t minLength, bool case_sensitive);
+    ~check_digit_identifier() override = default;
+    check_digit_identifier(const check_digit_identifier &) = delete;
+    check_digit_identifier(check_digit_identifier &&) noexcept = default;
+    check_digit_identifier &operator=(const check_digit_identifier &) = delete;
+    check_digit_identifier &operator=(check_digit_identifier &&) noexcept = default;
+
+protected:
+    [[nodiscard]] std::string_view to_string_impl() const { return regex->pattern(); }
+    static constexpr bool is_supported_type_impl(DDWAF_OBJ_TYPE type)
+    {
+        return type == DDWAF_OBJ_STRING;
+    }
+
+    [[nodiscard]] std::pair<bool, dynamic_string> match_impl(std::string_view pattern) const;
+
+    check_digit_algorithm cda_;
+    std::unique_ptr<re2::RE2> regex{nullptr};
+    std::size_t min_length;
+
+    friend class base_impl<check_digit_identifier>;
+};
+
+} // namespace ddwaf::matcher
