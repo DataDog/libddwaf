2bcc91b00eba3e998ca1aa0009fe67f40a21ea1d
Anil Mahtani <929854+Anilm3@users.noreply.github.com>
2025-09-14 17:33:10 +0100

    Simplify memory resource

--- src/memory_resource.hpp
+++ src/memory_resource.hpp
@@ -6,22 +6,8 @@
 
 #pragma once
 
+#include <memory_resource>
 #include <version>
-
-#if defined(__cpp_lib_memory_resource)
-#  include <memory_resource>
-#else
-#  include <experimental/memory_resource>
-#  include <string>
-#  include <unordered_map>
-#  include <unordered_set>
-#  include <vector>
-
-#  if !defined(__cpp_lib_experimental_memory_resources)
-#    include "libcxx-compat/monotonic_buffer_resource.hpp"
-#  endif
-
-#endif
 
 namespace ddwaf::memory {
 
