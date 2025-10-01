52db4a4802a770953a0512c0d7bc10717a362e78
Anil Mahtani <929854+Anilm3@users.noreply.github.com>
2025-10-01 16:17:13 +0100

    Fixes and more tests

--- src/builder/ruleset_builder.cpp
+++ src/builder/ruleset_builder.cpp
@@ -247,7 +247,11 @@ std::shared_ptr<ruleset> ruleset_builder::build(
 
     if (contains(current_changes, change_set::obfuscator)) {
         obfuscator_ = global_config.obfuscator;
-    } else if (obfuscator_ == nullptr) {
+    }
+
+    // Ensure the obfuscator is never nullptr
+    // TODO: update the serializer to accept a nullptr obfuscator
+    if (obfuscator_ == nullptr) {
         obfuscator_ = std::make_shared<match_obfuscator>();
     }
 
--- tests/integration/events/obfuscator/test.cpp
+++ tests/integration/events/obfuscator/test.cpp
@@ -733,4 +733,90 @@ TEST(TestObfuscatorIntegration, TestInvalidConfigValue)
     ddwaf_destroy(handle);
 }
 
+TEST(TestObfuscatorIntegration, RemoveObfuscator)
+{
+    // Verify that when the obfuscator is removed, everything still works as expected
+    auto *alloc = ddwaf_get_default_allocator();
+
+    auto rule = read_file<ddwaf_object>("obfuscator.yaml", base_dir);
+    ASSERT_TRUE(rule.type != DDWAF_OBJ_INVALID);
+
+    auto obfuscator =
+        yaml_to_object<ddwaf_object>("{obfuscator: {key_regex: 'password', value_regex: ''}}");
+    ASSERT_TRUE(obfuscator.type != DDWAF_OBJ_INVALID);
+
+    ddwaf_builder builder = ddwaf_builder_init();
+    ddwaf_builder_add_or_update_config(builder, STRL("rules"), &rule, nullptr);
+    ddwaf_builder_add_or_update_config(builder, STRL("obfuscator"), &obfuscator, nullptr);
+    ddwaf_handle handle = ddwaf_builder_build_instance(builder);
+    ASSERT_NE(handle, nullptr);
+
+    ddwaf_object_destroy(&rule, alloc);
+    ddwaf_object_destroy(&obfuscator, alloc);
+
+    {
+        ddwaf_context context = ddwaf_context_init(handle, alloc);
+
+        ddwaf_object parameter;
+        ddwaf_object_set_map(&parameter, 1, alloc);
+
+        auto *inter = ddwaf_object_insert_key(&parameter, STRL("value"), alloc);
+        ddwaf_object_set_map(inter, 1, alloc);
+        ddwaf_object_set_string(
+            ddwaf_object_insert_key(inter, STRL("passwordle"), alloc), STRL("rule1"), alloc);
+
+        ddwaf_object out;
+        EXPECT_EQ(ddwaf_context_eval(context, &parameter, alloc, &out, LONG_TIME), DDWAF_MATCH);
+        EXPECT_EVENTS(out, {.id = "1",
+                               .name = "rule1",
+                               .tags = {{"type", "security_scanner"}, {"category", "category1"}},
+                               .matches = {{.op = "match_regex",
+                                   .op_value = "rule1",
+                                   .highlight = "<Redacted>"sv,
+                                   .args = {{
+                                       .value = "<Redacted>"sv,
+                                       .address = "value",
+                                       .path = {"passwordle"},
+                                   }}}}});
+        ddwaf_object_destroy(&out, alloc);
+        ddwaf_context_destroy(context);
+    }
+
+    ddwaf_destroy(handle);
+
+    ddwaf_builder_remove_config(builder, STRL("obfuscator"));
+    handle = ddwaf_builder_build_instance(builder);
+    ddwaf_builder_destroy(builder);
+
+    {
+        ddwaf_context context = ddwaf_context_init(handle, alloc);
+
+        ddwaf_object parameter;
+        ddwaf_object_set_map(&parameter, 1, alloc);
+
+        auto *inter = ddwaf_object_insert_key(&parameter, STRL("value"), alloc);
+        ddwaf_object_set_map(inter, 1, alloc);
+        ddwaf_object_set_string(
+            ddwaf_object_insert_key(inter, STRL("passwordle"), alloc), STRL("rule1"), alloc);
+
+        ddwaf_object out;
+        EXPECT_EQ(ddwaf_context_eval(context, &parameter, alloc, &out, LONG_TIME), DDWAF_MATCH);
+        EXPECT_EVENTS(out, {.id = "1",
+                               .name = "rule1",
+                               .tags = {{"type", "security_scanner"}, {"category", "category1"}},
+                               .matches = {{.op = "match_regex",
+                                   .op_value = "rule1",
+                                   .highlight = "rule1"sv,
+                                   .args = {{
+                                       .value = "rule1"sv,
+                                       .address = "value",
+                                       .path = {"passwordle"},
+                                   }}}}});
+        ddwaf_object_destroy(&out, alloc);
+        ddwaf_context_destroy(context);
+    }
+
+    ddwaf_destroy(handle);
+}
+
 } // namespace
