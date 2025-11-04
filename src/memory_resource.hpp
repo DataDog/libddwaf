315d49e2947ba55f807c1a6c4a67a0e83ac79cee
Anil Mahtani <929854+Anilm3@users.noreply.github.com>
2025-11-04 18:28:00 +0000

    Increase MacOS target to 14.2.1

--- src/memory_resource.hpp
+++ src/memory_resource.hpp
@@ -7,29 +7,9 @@
 #pragma once
 
 #include <cstddef>
+#include <memory_resource>
 #include <new>
 #include <version>
-
-#if defined(__cpp_lib_memory_resource)
-#  include <memory_resource>
-#else
-#  include <experimental/memory_resource>
-#  include <string>
-#  include <unordered_map>
-#  include <unordered_set>
-#  include <vector>
-
-#  if !defined(__cpp_lib_experimental_memory_resources)
-#    include "libcxx-compat/monotonic_buffer_resource.hpp"
-#    include "libcxx-compat/synchronized_pool_resource.hpp"
-#    include "libcxx-compat/unsynchronized_pool_resource.hpp"
-
-namespace std { // NOLINT(cert-dcl58-cpp)
-namespace pmr = std::experimental::pmr;
-} // namespace std
-
-#  endif
-#endif
 
 namespace ddwaf::memory {
 
